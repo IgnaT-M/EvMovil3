60abd6d7b99e8d904f5dc1dfcfcc8beffeb18319
bjimenezponce <bjimenezponce1@gmail.com>
2024-12-10 21:48:24 -0300

    commit de actualizaciones

--- src/pages/lista_tareas/TodoList.tsx
+++ src/pages/lista_tareas/TodoList.tsx
@@ -18,7 +18,7 @@ import {
   IonGrid,
   IonRow,
   IonCol,
-  IonImg
+  IonImg,
 } from "@ionic/react";
 import {
   trash,
@@ -31,7 +31,6 @@ import {
   closeCircleOutline,
 } from "ionicons/icons";
 import "./TodoList.css";
-
 
 import Peripherals from "../../components/peripherals/camera/Peripherals";
 
@@ -105,183 +104,188 @@ const MyList: React.FC = () => {
 
   return (
     <>
-    <IonPage>
-      <IonHeader>
-        
-        <IonToolbar className="page-header">
-          <IonTitle
-            style={{
-              textAlign: "center",
-              width: "100%",
-              fontSize: "1.5rem",
-            }}
-           >Mi Lista</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        {/* Toast Messages */}
-        <IonToast
-          isOpen={showSuccessToast}
-          position="top"
-          onDidDismiss={() => setShowSuccessToast(false)}
-          message="¡Objeto agregado correctamente!"
-          duration={2000}
-          color="success"
-          icon={flameSharp}
-        />
-        <IonToast
-          isOpen={showErrorToast}
-          onDidDismiss={() => setShowErrorToast(false)}
-          message="Por favor, completa todos los campos."
-          duration={2000}
-          color="danger"
-          icon={thumbsDown}
-        />
-        <IonToast
-          isOpen={showUpdateToast}
-          onDidDismiss={() => setShowUpdateToast(false)}
-          message="Objeto actualizado correctamente."
-          duration={2000}
-          color="tertiary"
-          position="top"
-          icon={thumbsUp}
-        />
-        <IonToast
-          isOpen={showDeleteToast}
-          onDidDismiss={() => setShowDeleteToast(false)}
-          message="Objeto eliminado."
-          duration={2000}
-          color="warning"
-          position="top"
-          icon={closeCircleOutline}
-        />
+      <IonPage>
+        <IonHeader>
+          <IonToolbar className="page-header">
+            <IonTitle
+              style={{
+                textAlign: "center",
+                width: "100%",
+                fontSize: "1.5rem",
+              }}
+            >
+              Mi Lista
+            </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          {/* Mesnsajes de los toast */}
+          <IonToast
+            isOpen={showSuccessToast}
+            position="top"
+            onDidDismiss={() => setShowSuccessToast(false)}
+            message="¡Objeto agregado correctamente!"
+            duration={2000}
+            color="success"
+            icon={flameSharp}
+          />
+          <IonToast
+            isOpen={showErrorToast}
+            onDidDismiss={() => setShowErrorToast(false)}
+            message="Por favor, completa todos los campos."
+            duration={2000}
+            color="danger"
+            icon={thumbsDown}
+          />
+          <IonToast
+            isOpen={showUpdateToast}
+            onDidDismiss={() => setShowUpdateToast(false)}
+            message="Objeto actualizado correctamente."
+            duration={2000}
+            color="tertiary"
+            position="top"
+            icon={thumbsUp}
+          />
+          <IonToast
+            isOpen={showDeleteToast}
+            onDidDismiss={() => setShowDeleteToast(false)}
+            message="Objeto eliminado."
+            duration={2000}
+            color="warning"
+            position="top"
+            icon={closeCircleOutline}
+          />
 
-        {/* Formulario para agregar un nuevo ítem */}
-        <IonCard>
-          <IonCardContent >
-            <IonGrid>
-              <IonRow>
-                <IonCol size="12">
-                  <IonItem>
-                    <IonLabel position="floating" style={{textAlign:"center", fontSize:"25px"}}>Nombre</IonLabel>
-                    <br />
-                    <IonInput
-                      value={name}
-                      onIonChange={(e) => setName(e.detail.value!)}
-                      placeholder="Agregar objeto"
-                      className="input-name"
-                    />
-                  </IonItem>
-                </IonCol>
-                <IonCol size="12">
-                  <IonItem>
-                    <IonLabel position="floating" style={{textAlign:"center", fontSize:"25px" }}>Descripción</IonLabel>
-                    <br />
-                    <IonInput
-                      value={description}
-                      onIonChange={(e) => setDescription(e.detail.value!)}
-                      placeholder="Agregar descripción"
-                      className="input-description"
-                    />
-                  </IonItem>
-                </IonCol>
-                <IonCol size="12" className="ion-text-center">
-                  <IonButton
-                  style={{marginTop: "20px"}}
-                    // color="primary"
-                    expand="block"
-                    onClick={addItem}
-                    className="add-button"
-
-                  >
-                    <IonIcon icon={addCircleOutline} slot="start" />
-                    Agregar objeto
-                  </IonButton>
-                </IonCol>
-
-              </IonRow>
-            </IonGrid>
-          </IonCardContent>
-        </IonCard>
-        
-
-
-
-        {/* Lista de ítems */}
-        <IonList>
-          
-          {items.map((item) => (
-            <IonCard key={item.id} className="card-item">
-              <IonCardContent className="card-content">
-                <IonItem lines="none">
-                  {item.isEditing ? (
-                    <>
-                      <IonInput
-                        value={item.name}
-                        placeholder="Edit name"
-                        className="edit-input"
-                        onIonChange={(e) =>
-                          setItems(
-                            items.map((i) =>
-                              i.id === item.id
-                                ? { ...i, name: e.detail.value! }
-                                : i
-                            )
-                          )
-                        }
-                      />
-                      <IonInput
-                        value={item.description}
-                        placeholder="Edit description"
-                        className="edit-input"
-                        onIonChange={(e) =>
-                          setItems(
-                            items.map((i) =>
-                              i.id === item.id
-                                ? { ...i, description: e.detail.value! }
-                                : i
-                            )
-                          )
-                        }
-                      />
-                      <IonButton
-                        color="success"
-                        fill="clear"
-                        onClick={() =>
-                          updateItem(item.id, item.name, item.description)
-                        }
+          {/* Formulario para agregar un nuevo ítem */}
+          <IonCard>
+            <IonCardContent>
+              <IonGrid>
+                <IonRow>
+                  <IonCol size="12">
+                    <IonItem>
+                      <IonLabel
+                        position="floating"
+                        style={{ textAlign: "center", fontSize: "25px" }}
                       >
-                        <IonIcon icon={checkmark} />
-                      </IonButton>
-                    </>
-                  ) : (
-                    <IonLabel className="item-label">
-                      <h2 className="item-title">{item.name}</h2>
-                      <p className="item-description">{item.description}</p>
-                    </IonLabel>
-                  )}
-                  <IonButton
-                    color="primary"
-                    fill="clear"
-                    onClick={() => toggleEdit(item.id)}
-                  >
-                    <IonIcon icon={create} />
-                  </IonButton>
-                  <IonButton
-                    color="danger"
-                    fill="clear"
-                    onClick={() => deleteItem(item.id)}
-                  >
-                    <IonIcon icon={trash} />
-                  </IonButton>
-                </IonItem>
-              </IonCardContent>
-            </IonCard>
-          ))}
-        </IonList>
-      </IonContent>
-    </IonPage>
-</>
+                        Nombre
+                      </IonLabel>
+                      <br />
+                      <IonInput
+                        value={name}
+                        onIonChange={(e) => setName(e.detail.value!)}
+                        placeholder="Agregar objeto"
+                        className="input-name"
+                      />
+                    </IonItem>
+                  </IonCol>
+                  <IonCol size="12">
+                    <IonItem>
+                      <IonLabel
+                        position="floating"
+                        style={{ textAlign: "center", fontSize: "25px" }}
+                      >
+                        Descripción
+                      </IonLabel>
+                      <br />
+                      <IonInput
+                        value={description}
+                        onIonChange={(e) => setDescription(e.detail.value!)}
+                        placeholder="Agregar descripción"
+                        className="input-description"
+                      />
+                    </IonItem>
+                  </IonCol>
+                  <IonCol size="12" className="ion-text-center">
+                    <IonButton
+                      style={{ marginTop: "20px" }}
+                      // color="primary"
+                      expand="block"
+                      onClick={addItem}
+                      className="add-button"
+                    >
+                      <IonIcon icon={addCircleOutline} slot="start" />
+                      Agregar objeto
+                    </IonButton>
+                  </IonCol>
+                </IonRow>
+              </IonGrid>
+            </IonCardContent>
+          </IonCard>
+
+          {/* Lista de ítems */}
+          <IonList>
+            {items.map((item) => (
+              <IonCard key={item.id} className="card-item">
+                <IonCardContent className="card-content">
+                  <IonItem lines="none">
+                    {item.isEditing ? (
+                      <>
+                        <IonInput
+                          value={item.name}
+                          placeholder="Edit name"
+                          className="edit-input"
+                          onIonChange={(e) =>
+                            setItems(
+                              items.map((i) =>
+                                i.id === item.id
+                                  ? { ...i, name: e.detail.value! }
+                                  : i
+                              )
+                            )
+                          }
+                        />
+                        <IonInput
+                          value={item.description}
+                          placeholder="Edit description"
+                          className="edit-input"
+                          onIonChange={(e) =>
+                            setItems(
+                              items.map((i) =>
+                                i.id === item.id
+                                  ? { ...i, description: e.detail.value! }
+                                  : i
+                              )
+                            )
+                          }
+                        />
+                        <IonButton
+                          color="success"
+                          fill="clear"
+                          onClick={() =>
+                            updateItem(item.id, item.name, item.description)
+                          }
+                        >
+                          <IonIcon icon={checkmark} />
+                        </IonButton>
+                      </>
+                    ) : (
+                      <IonLabel className="item-label">
+                        <h2 className="item-title">{item.name}</h2>
+                        <p className="item-description">{item.description}</p>
+                      </IonLabel>
+                    )}
+                    <IonButton
+                      color="primary"
+                      fill="clear"
+                      onClick={() => toggleEdit(item.id)}
+                    >
+                      <IonIcon icon={create} />
+                    </IonButton>
+                    <IonButton
+                      color="danger"
+                      fill="clear"
+                      onClick={() => deleteItem(item.id)}
+                    >
+                      <IonIcon icon={trash} />
+                    </IonButton>
+                  </IonItem>
+                </IonCardContent>
+              </IonCard>
+            ))}
+          </IonList>
+        </IonContent>
+      </IonPage>
+    </>
   );
 };
 
